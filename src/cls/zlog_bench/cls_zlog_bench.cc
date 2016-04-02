426e6a0c5624e24339fe621d916f1a81c542b4e2
Noah Watkins <noahwatkins@gmail.com>
2016-04-02 16:51:44 -0700

    zlog_bench: add append_check_epoch
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- src/cls/zlog_bench/cls_zlog_bench.cc
+++ src/cls/zlog_bench/cls_zlog_bench.cc
@@ -2,11 +2,28 @@
 #include "cls_zlog_bench_ops.h"
 #include "cls_zlog_bench_client.h"
 
+#define ZLOG_PRINT_DEBUG
+
 CLS_VER(1,0)
 CLS_NAME(zlog_bench)
 
 cls_handle_t h_class;
 cls_method_handle_t h_append;
+cls_method_handle_t h_append_init;
+cls_method_handle_t h_append_check_epoch;
+
+#define ZLOG_EPOCH_KEY "____zlog.epoch"
+#define ZLOG_POS_PREFIX "____zlog.pos."
+
+/*
+ * Convert value into zero-padded string for omap comparisons.
+ */
+static inline std::string u64tostr(uint64_t value)
+{
+  std::stringstream ss;
+  ss << ZLOG_POS_PREFIX << std::setw(20) << std::setfill('0') << value;
+  return ss.str();
+}
 
 /*
  * Overhead-free log entry append to stream.
@@ -35,6 +52,130 @@ static int append(cls_method_context_t hctx, bufferlist *in, bufferlist *out)
     return ret;
   }
 
+#ifdef ZLOG_PRINT_DEBUG
+  CLS_LOG(0, "APPEND NO INDEX: %llu %llu %llu\n",
+      op.epoch,
+      op.position,
+      op.data.length());
+#endif
+
+  return 0;
+}
+
+static int get_epoch(cls_method_context_t hctx, uint64_t *pepoch)
+{
+  bufferlist bl;
+  int ret = cls_cxx_map_get_val(hctx, ZLOG_EPOCH_KEY, &bl);
+  if (ret < 0)
+    return ret;
+
+  uint64_t cur_epoch;
+  try {
+    bufferlist::iterator it = bl.begin();
+    ::decode(cur_epoch, it);
+  } catch (buffer::error& err) {
+    CLS_LOG(0, "ERROR: get_epoch(): failed to decode epoch entry");
+    return -EIO;
+  }
+
+  if (pepoch)
+    *pepoch = cur_epoch;
+
+  return 0;
+}
+
+static int check_epoch(cls_method_context_t hctx, uint64_t epoch)
+{
+  uint64_t cur_epoch;
+  int ret = get_epoch(hctx, &cur_epoch);
+  if (ret < 0) {
+    if (ret == -ENOENT) {
+      CLS_ERR("ERROR: check_epoch: failed to read epoch... init?");
+      return ret;
+    }
+    CLS_ERR("ERROR: check_epoch(): failed to read epoch (%d)", ret);
+    return ret;
+  }
+  assert(ret == 0);
+
+  if (epoch <= cur_epoch) {
+    CLS_LOG(0, "NOTICE: check_update(): old epoch proposed");
+    return -EINVAL;
+  }
+
+  return 0;
+}
+
+static int append_init(cls_method_context_t hctx, bufferlist *in, bufferlist *out)
+{
+  int ret = get_epoch(hctx, NULL);
+  if (ret != -ENOENT) {
+    if (ret == 0) {
+      CLS_ERR("ERROR: append_init: received multiple init reqs!");
+      return -EIO;
+    }
+    CLS_ERR("ERROR: append_init: unexpected return value: %d", ret);
+    return ret;
+  }
+  assert(ret == -ENOENT);
+
+  uint64_t new_epoch = 10;
+  bufferlist bl;
+  ::encode(new_epoch, bl);
+
+  ret = cls_cxx_map_set_val(hctx, ZLOG_EPOCH_KEY, &bl);
+  if (ret < 0) {
+    CLS_ERR("ERROR: check_epoch: failed to initialize object");
+    return ret;
+  }
+
+#ifdef ZLOG_PRINT_DEBUG
+  CLS_LOG(0, "APPEND INIT NEW EPOCH = 10");
+#endif
+
+  return 0;
+}
+
+/*
+ * Stream append with epoch guard.
+ */
+static int append_check_epoch(cls_method_context_t hctx, bufferlist *in, bufferlist *out)
+{
+  cls_zlog_bench_append_op op;
+  try {
+    bufferlist::iterator it = in->begin();
+    ::decode(op, it);
+  } catch (buffer::error& err) {
+    CLS_LOG(0, "ERROR: append(): failed to decode input");
+    return -EINVAL;
+  }
+
+  int ret = check_epoch(hctx, op.epoch);
+  if (ret) {
+    CLS_ERR("NOTICE: append_check_epoch(): stale epoch value");
+    return ret;
+  }
+
+  uint64_t size;
+  ret = cls_cxx_stat(hctx, &size, NULL);
+  if (ret) {
+    CLS_ERR("ERROR: append_check_epoch: stat error: %d", ret);
+    return ret;
+  }
+
+  ret = cls_cxx_write(hctx, size, op.data.length(), &op.data);
+  if (ret) {
+    CLS_ERR("ERROR: append_check_epoch: write error: %d", ret);
+    return ret;
+  }
+
+#ifdef ZLOG_PRINT_DEBUG
+  CLS_LOG(0, "APPEND CHECK EPOCH: %llu %llu %llu\n",
+      op.epoch,
+      op.position,
+      op.data.length());
+#endif
+
   return 0;
 }
 
@@ -47,4 +188,12 @@ void __cls_init()
   cls_register_cxx_method(h_class, "append",
                           CLS_METHOD_RD | CLS_METHOD_WR,
                           append, &h_append);
+
+  cls_register_cxx_method(h_class, "append_check_epoch",
+                          CLS_METHOD_RD | CLS_METHOD_WR,
+                          append_check_epoch, &h_append_check_epoch);
+
+  cls_register_cxx_method(h_class, "append_init",
+                          CLS_METHOD_RD | CLS_METHOD_WR,
+                          append_init, &h_append_init);
 }
--- src/test/cls_zlog_bench/test_cls_zlog_bench.cc
+++ src/test/cls_zlog_bench/test_cls_zlog_bench.cc
@@ -29,9 +29,9 @@ TEST(ClsZlogBench, Append) {
   data.append(buf, sizeof(buf));
 
   // append #1
-  librados::ObjectWriteOperation op;
-  zlog_bench::cls_zlog_bench_append(op, 123, 456, data);
-  ret = ioctx.operate("oid", &op);
+  librados::ObjectWriteOperation *op = new librados::ObjectWriteOperation;
+  zlog_bench::cls_zlog_bench_append(*op, 123, 456, data);
+  ret = ioctx.operate("oid", op);
   ASSERT_EQ(ret, 0);
 
   ret = ioctx.stat("oid", &size, NULL);
@@ -41,8 +41,92 @@ TEST(ClsZlogBench, Append) {
   // append #2
   data.clear();
   data.append(buf, sizeof(buf));
-  zlog_bench::cls_zlog_bench_append(op, 123, 457, data);
-  ret = ioctx.operate("oid", &op);
+  op = new librados::ObjectWriteOperation;
+  zlog_bench::cls_zlog_bench_append(*op, 123, 457, data);
+  ret = ioctx.operate("oid", op);
+  ASSERT_EQ(ret, 0);
+
+  ret = ioctx.stat("oid", &size, NULL);
+  ASSERT_EQ(ret, 0);
+  ASSERT_EQ(size, 2*data.length());
+
+  ASSERT_EQ(0, destroy_one_pool_pp(pool_name, cluster));
+}
+
+TEST(ClsZlogBench, AppendCheckEpoch) {
+  librados::Rados cluster;
+  std::string pool_name = get_temp_pool_name();
+  ASSERT_EQ("", create_one_pool_pp(pool_name, cluster));
+  librados::IoCtx ioctx;
+  cluster.ioctx_create(pool_name.c_str(), ioctx);
+
+  int ret = ioctx.create("oid", true);
+  ASSERT_EQ(ret, 0);
+
+  uint64_t size;
+  ret = ioctx.stat("oid", &size, NULL);
+  ASSERT_EQ(ret, 0);
+  ASSERT_EQ(size, 0ULL);
+
+  char buf[1024];
+  ceph::bufferlist data;
+  data.append(buf, sizeof(buf));
+
+  // append failure
+  librados::ObjectWriteOperation *op = new librados::ObjectWriteOperation;
+  zlog_bench::cls_zlog_bench_append_check_epoch(*op, 9, 456, data);
+  ret = ioctx.operate("oid", op);
+  ASSERT_EQ(ret, -ENOENT);
+
+  // omap should be empty
+  std::map<std::string, ceph::bufferlist> kvs;
+  ret = ioctx.omap_get_vals("oid", "", 100, &kvs);
+  ASSERT_EQ(ret, 0);
+  ASSERT_EQ(kvs.size(), (unsigned)0);
+
+  // init stored epoch
+  op = new librados::ObjectWriteOperation;
+  zlog_bench::cls_zlog_bench_append_init(*op);
+  ret = ioctx.operate("oid", op);
+  ASSERT_EQ(ret, 0);
+
+  // init will set the epoch in the omap
+  ret = ioctx.omap_get_vals("oid", "", 100, &kvs);
+  ASSERT_EQ(ret, 0);
+  ASSERT_EQ(kvs.size(), (unsigned)1);
+  ASSERT_TRUE(kvs.find("____zlog.epoch") != kvs.end());
+
+  // append #1 fails because old epoch
+  data.clear();
+  data.append(buf, sizeof(buf));
+  op = new librados::ObjectWriteOperation;
+  zlog_bench::cls_zlog_bench_append_check_epoch(*op, 9, 456, data);
+  ret = ioctx.operate("oid", op);
+  ASSERT_EQ(ret, -EINVAL);
+
+  // object size is still 0
+  ret = ioctx.stat("oid", &size, NULL);
+  ASSERT_EQ(ret, 0);
+  ASSERT_EQ(size, 0);
+
+  // append #1
+  data.clear();
+  data.append(buf, sizeof(buf));
+  op = new librados::ObjectWriteOperation;
+  zlog_bench::cls_zlog_bench_append_check_epoch(*op, 123, 456, data);
+  ret = ioctx.operate("oid", op);
+  ASSERT_EQ(ret, 0);
+
+  ret = ioctx.stat("oid", &size, NULL);
+  ASSERT_EQ(ret, 0);
+  ASSERT_EQ(size, data.length());
+
+  // append #2
+  data.clear();
+  data.append(buf, sizeof(buf));
+  op = new librados::ObjectWriteOperation;
+  zlog_bench::cls_zlog_bench_append_check_epoch(*op, 123, 457, data);
+  ret = ioctx.operate("oid", op);
   ASSERT_EQ(ret, 0);
 
   ret = ioctx.stat("oid", &size, NULL);
