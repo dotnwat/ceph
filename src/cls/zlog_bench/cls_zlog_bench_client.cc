9cfcf3f7fdee26036338c69f2218a9bcc1fb0f1a
Noah Watkins <noahwatkins@gmail.com>
2016-04-02 16:51:44 -0700

    zlog_bench: add append_check_epoch
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- src/cls/zlog_bench/cls_zlog_bench_client.cc
+++ src/cls/zlog_bench/cls_zlog_bench_client.cc
@@ -16,4 +16,22 @@ namespace zlog_bench {
     ::encode(call, in);
     op.exec("zlog_bench", "append", in);
   }
+
+  void cls_zlog_bench_append_init(librados::ObjectWriteOperation& op)
+  {
+    bufferlist in;
+    op.exec("zlog_bench", "append_init", in);
+  }
+
+  void cls_zlog_bench_append_check_epoch(librados::ObjectWriteOperation& op,
+      uint64_t epoch, uint64_t position, ceph::bufferlist& data)
+  {
+    bufferlist in;
+    cls_zlog_bench_append_op call;
+    call.epoch = epoch;
+    call.position = position;
+    call.data = data;
+    ::encode(call, in);
+    op.exec("zlog_bench", "append_check_epoch", in);
+  }
 }
--- src/cls/zlog_bench/cls_zlog_bench_client.h
+++ src/cls/zlog_bench/cls_zlog_bench_client.h
@@ -10,6 +10,11 @@
 namespace zlog_bench {
   void cls_zlog_bench_append(librados::ObjectWriteOperation& op, uint64_t epoch,
       uint64_t position, ceph::bufferlist& data);
+
+  void cls_zlog_bench_append_init(librados::ObjectWriteOperation& op);
+
+  void cls_zlog_bench_append_check_epoch(librados::ObjectWriteOperation& op,
+      uint64_t epoch, uint64_t position, ceph::bufferlist& data);
 }
 
 #endif
