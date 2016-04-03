343dddcd3588105311557b09895d188444e32764
Noah Watkins <noahwatkins@gmail.com>
2016-04-03 16:33:08 -0700

    wip
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- src/cls/zlog_bench/cls_zlog_bench.cc
+++ src/cls/zlog_bench/cls_zlog_bench.cc
@@ -2,7 +2,7 @@
 #include "cls_zlog_bench_ops.h"
 #include "cls_zlog_bench_client.h"
 
-#define ZLOG_PRINT_DEBUG
+//#define ZLOG_PRINT_DEBUG
 
 CLS_VER(1,0)
 CLS_NAME(zlog_bench)
@@ -16,6 +16,9 @@ cls_method_handle_t h_append_check_epoch;
 
 cls_method_handle_t h_map_write_null;
 cls_method_handle_t h_map_write_full;
+
+cls_method_handle_t h_stream_write_null;
+cls_method_handle_t h_stream_write_full;
 
 #define ZLOG_EPOCH_KEY "____zlog.epoch"
 #define ZLOG_POS_PREFIX "____zlog.pos."
@@ -62,9 +65,9 @@ static int append(cls_method_context_t hctx, bufferlist *in, bufferlist *out)
 
 #ifdef ZLOG_PRINT_DEBUG
   CLS_LOG(0, "APPEND NO INDEX: %llu %llu %llu\n",
-      op.epoch,
-      op.position,
-      op.data.length());
+      (unsigned long long)op.epoch,
+      (unsigned long long)op.position,
+      (unsigned long long)op.data.length());
 #endif
 
   return 0;
@@ -179,9 +182,9 @@ static int append_check_epoch(cls_method_context_t hctx, bufferlist *in, bufferl
 
 #ifdef ZLOG_PRINT_DEBUG
   CLS_LOG(0, "APPEND CHECK EPOCH: %llu %llu %llu\n",
-      op.epoch,
-      op.position,
-      op.data.length());
+      (unsigned long long)op.epoch,
+      (unsigned long long)op.position,
+      (unsigned long long)op.data.length());
 #endif
 
   return 0;
@@ -281,10 +284,10 @@ static int append_omap_index(cls_method_context_t hctx, bufferlist *in, bufferli
 
 #ifdef ZLOG_PRINT_DEBUG
   CLS_LOG(0, "APPEND OMAP INDEX: %llu %llu %llu %llu\n",
-      op.epoch,
-      op.position,
-      md.offset,
-      md.length);
+      (unsigned long long)op.epoch,
+      (unsigned long long)op.position,
+      (unsigned long long)md.offset,
+      (unsigned long long)md.length);
 #endif
 
     return 0;
@@ -318,9 +321,9 @@ static int map_write_null(cls_method_context_t hctx, bufferlist *in, bufferlist 
 
 #ifdef ZLOG_PRINT_DEBUG
   CLS_LOG(0, "MAP WRITE NULL: %llu %llu %llu\n",
-      op.epoch,
-      op.position,
-      op.data.length());
+      (unsigned long long)op.epoch,
+      (unsigned long long)op.position,
+      (unsigned long long)op.data.length());
 #endif
 
   return 0;
@@ -398,9 +401,136 @@ static int map_write_full(cls_method_context_t hctx, bufferlist *in, bufferlist 
 
 #ifdef ZLOG_PRINT_DEBUG
   CLS_LOG(0, "MAP WRITE FULL: %llu %llu %llu\n",
-      op.epoch,
-      op.position,
-      op.data.length());
+      (unsigned long long)op.epoch,
+      (unsigned long long)op.position,
+      (unsigned long long)op.data.length());
+#endif
+
+    return 0;
+  }
+
+  // this would be an error related to the write failing because the log entry
+  // position had already been written, filled, or trimmed.
+  return -EROFS;
+}
+
+/*
+ * Overhead-free stream write.
+ */
+static int stream_write_null(cls_method_context_t hctx, bufferlist *in, bufferlist *out)
+{
+  cls_zlog_bench_append_op op;
+  try {
+    bufferlist::iterator it = in->begin();
+    ::decode(op, it);
+  } catch (buffer::error& err) {
+    CLS_LOG(0, "ERROR: stream write null: failed to decode input");
+    return -EINVAL;
+  }
+
+  /*
+   * A design would either communicate the logical position, or the offset of
+   * the write (probably the later). Here we avoid creating an entirely new
+   * data structure and just re-use op.position for the write offset.
+   */
+  int ret = cls_cxx_write(hctx, op.position, op.data.length(), &op.data);
+  if (ret) {
+    CLS_ERR("ERROR: stream write null: write error: %d", ret);
+    return ret;
+  }
+
+#ifdef ZLOG_PRINT_DEBUG
+  CLS_LOG(0, "STREAM WRITE NULL: %llu %llu %llu\n",
+      (unsigned long long)op.epoch,
+      (unsigned long long)op.position,
+      (unsigned long long)op.data.length());
+#endif
+
+  return 0;
+}
+
+struct cls_zlog_log_entry_small_md {
+  char flags;
+
+  cls_zlog_log_entry_small_md() {}
+
+  void encode(bufferlist& bl) const {
+    ENCODE_START(1, 1, bl);
+    ::encode(flags, bl);
+    ENCODE_FINISH(bl);
+  }
+
+  void decode(bufferlist::iterator& bl) {
+    DECODE_START(1, bl);
+    ::decode(flags, bl);
+    DECODE_FINISH(bl);
+  }
+};
+WRITE_CLASS_ENCODER(cls_zlog_log_entry_small_md)
+
+/*
+ * Stream write with explicit offset
+ */
+static int stream_write_full(cls_method_context_t hctx, bufferlist *in, bufferlist *out)
+{
+  cls_zlog_bench_append_op op;
+  try {
+    bufferlist::iterator it = in->begin();
+    ::decode(op, it);
+  } catch (buffer::error& err) {
+    CLS_LOG(0, "ERROR: stream write full: failed to decode input");
+    return -EINVAL;
+  }
+
+  int ret = check_epoch(hctx, op.epoch);
+  if (ret) {
+    CLS_ERR("NOTICE: stream write full: stale epoch value");
+    return ret;
+  }
+
+  /*
+   * in this case we are keeping the index in terms of the phyiscal offset
+   * rather than the logical entry position.
+   */
+  bufferlist bl;
+  std::string key = u64tostr(op.position);
+  ret = cls_cxx_map_get_val(hctx, key, &bl);
+  if (ret < 0 && ret != -ENOENT) {
+    CLS_LOG(0, "ERROR: stream write full: failed to read index");
+    return ret;
+  }
+
+  // if position hasn't been written, we'll take it
+  if (ret == -ENOENT) {
+
+    // append the log entry data
+    ret = cls_cxx_write(hctx, op.position, op.data.length(), &op.data);
+    if (ret) {
+      CLS_ERR("ERROR: stream write full: write error: %d", ret);
+      return ret;
+    }
+
+    // metadata is just about the entry state... no logical to physical
+    // mapping metadata is necessary
+    cls_zlog_log_entry_small_md md;
+    bufferlist mdbl;
+    ::encode(md, mdbl);
+    ret = cls_cxx_map_set_val(hctx, key, &mdbl);
+    if (ret < 0) {
+      CLS_LOG(0, "ERROR: stream write full: failed to write index");
+      return ret;
+    }
+
+    // update max position? nope. in zlog we would update the current max
+    // position. but it should be much more efficient to spend some time
+    // figuring out what hte maximum position is when we need to, which is
+    // relatively infrequent.
+
+#ifdef ZLOG_PRINT_DEBUG
+  CLS_LOG(0, "STREAM WRITE FULL: %llu %llu %llu\n",
+      (unsigned long long)op.epoch,
+      (unsigned long long)op.position,
+      (unsigned long long)op.data.length());
 #endif
 
     return 0;
@@ -440,4 +570,12 @@ void __cls_init()
   cls_register_cxx_method(h_class, "map_write_full",
                           CLS_METHOD_RD | CLS_METHOD_WR,
                           map_write_full, &h_map_write_full);
+
+  cls_register_cxx_method(h_class, "stream_write_null",
+                          CLS_METHOD_RD | CLS_METHOD_WR,
+                          stream_write_null, &h_stream_write_null);
+
+  cls_register_cxx_method(h_class, "stream_write_full",
+                          CLS_METHOD_RD | CLS_METHOD_WR,
+                          stream_write_full, &h_stream_write_full);
 }
--- src/cls/zlog_bench/cls_zlog_bench_client.h
+++ src/cls/zlog_bench/cls_zlog_bench_client.h
@@ -24,6 +24,12 @@ namespace zlog_bench {
 
   void cls_zlog_bench_map_write_full(librados::ObjectWriteOperation& op,
       uint64_t epoch, uint64_t position, ceph::bufferlist& data);
+
+  void cls_zlog_bench_stream_write_null(librados::ObjectWriteOperation& op,
+      uint64_t epoch, uint64_t position, ceph::bufferlist& data);
+
+  void cls_zlog_bench_stream_write_full(librados::ObjectWriteOperation& op,
+      uint64_t epoch, uint64_t position, ceph::bufferlist& data);
 }
 
 #endif
